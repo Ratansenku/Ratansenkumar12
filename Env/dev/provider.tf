2f91388d4df7bd5f72c66342caf735b734995800
ratansen kumar <ratansenkumar@gmail.com>
2025-03-17 14:07:14 +0530

    add

--- Env/dev/provider.tf
+++ Env/dev/provider.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.42.0"
-    }
-  }
-}
 provider "azurerm" {
   features {}
   subscription_id = "1fe49dcb-216b-4a9c-a412-afa7742cee3d"
